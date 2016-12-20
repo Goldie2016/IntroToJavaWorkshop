ca8d27cc3e45f6cad4c7ad3f86b075329c5e9bdb
Love4653 <scerruti@otabi.com>
2016-12-20 11:56:48 -0800

    day 2

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,35 @@
 package day1.robot;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot rick=new Robot(400,400);
+rick.penDown();
+int sides=0;
+while(sides<10){
+rick.move(400);
+rick.turn(200);
+sides=sides+1;
+}
 
-	
-		
+Robot carl=new Robot();
+carl.penDown();
+sides=0;
+while(sides<4){
+carl.move(100);
+carl.turn(90);
+sides=sides+1;
+
+}
+
+
+
 		
 	}
 }
--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -10,28 +10,30 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
+Robot firestar=new Robot();
 
 		// 3. Put the robot's pen down
-
+firestar.penDown();
 		// 8. Make the robot go at maximum speed (10)
-
+firestar.setSpeed(10);
 		// 9. Set the pen to a color that you like for the shape
-
+firestar.setPenColor(Color.ORANGE);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+int i=0;
+int angle=72;
 		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
+while(i<200){
 		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
 
 			// 2. Move the robot 200 pixels
-	
+firestar.move(i);
 			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
 	
 			// 6. Turn the robot the amount in your angle variable
-	
+	firestar.turn(angle);
 			// 11. Turn the robot one more degree
-
-	}
+i=i+1;
+	}}
 
 	// Variations:
 	// *12. make the pattern really huge
